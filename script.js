4c821c90ed522cb024390b4240e1cb7d4dc32499
AmithAKulk <95955499+AmithAKulk@users.noreply.github.com>
2023-11-27 20:51:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-function getFormvalue() {
-    //Write your code here
+document.getElementById("myForm").addEventListener("submit", function(event) {
+      event.preventDefault(); // Prevent the default form submission behavior
 
-}
+      // Get form data
+      const formData = new FormData(this);
+      let result = "";
+
+      // Construct the result string
+      for (let [key, value] of formData.entries()) {
+        result += key + ": " + value + "\n";
+      }
+
+      // Display the result using alert with the specified format
+      alert(result);
+    });
